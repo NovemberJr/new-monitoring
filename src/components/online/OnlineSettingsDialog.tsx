7c97db6443a7f23c218432580d702bd923170efe
NovemberJr <jr.november@gmail.com>
2026-03-23 14:43:16 +0700

    added display select placeholder

--- src/components/online/OnlineSettingsDialog.tsx
+++ src/components/online/OnlineSettingsDialog.tsx
@@ -1,16 +1,33 @@
 import {
     Dialog,
-    // DialogClose,
+    DialogClose,
     DialogContent,
-    DialogDescription,
+    // DialogDescription,
     // DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog"
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
 import { Settings } from "lucide-react"
+import { Button } from "../ui/button"
 
 interface Props {}
+
+const items = [
+    { label: "Выберите экран", value: null },
+    { label: "TV1", value: "1" },
+    { label: "TV2", value: "2" },
+    { label: "TV3", value: "3" },
+]
 
 export function OnlineSettingsDialog({}: Props) {
     return (
@@ -18,7 +35,29 @@ export function OnlineSettingsDialog({}: Props) {
             <DialogTrigger>
                 <Settings className="h-6 cursor-pointer" />
             </DialogTrigger>
-            <DialogContent className="sm:max-w-sm"></DialogContent>
+            <DialogContent className="sm:max-w-sm">
+                <DialogHeader>
+                    <DialogTitle>Выберите экран</DialogTitle>
+                </DialogHeader>
+                <Select items={items}>
+                    <SelectTrigger className="w-full max-w-48">
+                        <SelectValue />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Экран</SelectLabel>
+                            {items.map((item) => (
+                                <SelectItem key={item.value} value={item.value}>
+                                    {item.label}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <DialogClose>
+                    <Button type="submit">Применить</Button>
+                </DialogClose>
+            </DialogContent>
         </Dialog>
     )
 }
